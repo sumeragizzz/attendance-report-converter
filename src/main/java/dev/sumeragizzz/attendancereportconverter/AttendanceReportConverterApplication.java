13c75c0db058c2ba42ca7d632e40283a87aee9f0
sumeragizzz <developer@sumeragizzz.dev>
2021-05-13 22:24:20 +0900

    Spring Initializrでプロジェクト作成

---
+++ src/main/java/dev/sumeragizzz/attendancereportconverter/AttendanceReportConverterApplication.java
@@ -0,0 +1,13 @@
+package dev.sumeragizzz.attendancereportconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceReportConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceReportConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sumeragizzz/attendancereportconverter/AttendanceReportConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sumeragizzz.attendancereportconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceReportConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
